0994339fb67e2c51aa353b70c181177579ba353e
deepthi-pt7569 <deepthi-pt7569@deepthi-pt7569.csez.zohocorpin.com>
2024-02-08 16:52:40 +0530

    new

---
+++ Stringbuffer/Stringbuff.java
@@ -0,0 +1,10 @@
+// String buffer using append()
+
+class stringbuff{
+public static void main(String[] args) {
+StringBuffer sb = new StringBuffer("zoho ");
+sb.append("cliq");
+System.out.println(sb);
+}
+}
+
