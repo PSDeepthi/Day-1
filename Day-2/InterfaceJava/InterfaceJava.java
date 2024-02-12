248347b31ae2b10b7ec143697af90fe6b8eb3c05
deepthi-pt7569 <deepthi-pt7569@deepthi-pt7569.csez.zohocorpin.com>
2024-02-12 15:26:47 +0530

    new

---
+++ Day-2/InterfaceJava/InterfaceJava.java
@@ -0,0 +1,25 @@
+interface Father{
+	abstract void call();
+	abstract void talk();
+}
+
+interface Mother{
+	abstract void call();
+	abstract void talk();
+}
+
+class InterfaceJava implements Father, Mother {
+	public void call(){
+	System.out.println("Calling");
+	}
+	public void talk(){
+	System.out.println("talking");
+	}
+	
+	public static void main(String[] args) {
+	InterfaceJava obj = new InterfaceJava();
+	obj.call();
+	obj.talk();
+	}
+
+} 
